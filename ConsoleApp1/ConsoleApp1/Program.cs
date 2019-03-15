d7e0d86050a211669b7bb563211c6ecef3dffb52
Odlanor <rft13hk@outlook.com>
2019-03-15 01:23:12 -0300

    Correções de Bugs
    Correções de bugs e implementação de testes unitários.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -21,12 +21,23 @@ namespace ConsoleApp1
         {
             char separator = Convert.ToChar(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator);
 
-            var strZeros = new String('0', (number + 2));
-            decimal MaxDecToCalc = Convert.ToDecimal(String.Concat("1", strZeros, separator, strZeros)) + value;
+            var valueAbs = Math.Abs(value);
+
+            int LenSizePart1 = valueAbs.ToString().IndexOf(separator);
+
+            if (LenSizePart1 == -1)
+            {
+                LenSizePart1 = valueAbs.ToString().Length;
+            }
+                        
+            string sinal = (valueAbs < 0 ? "-1" : "1");
+
+            var strZeros = new String('0', (LenSizePart1 + 2));
+            decimal MaxDecToCalc = Convert.ToDecimal(String.Concat(sinal, strZeros, separator, strZeros)) + valueAbs;
 
             if (MaxDecToCalc.ToString().IndexOf(separator) == -1)
             {
-                return value;
+                return valueAbs;
             }
 
             var lst = MaxDecToCalc.ToString().Split(separator);
@@ -40,14 +51,15 @@ namespace ConsoleApp1
 
             int p = stringConcat.Length;
 
-            while ((stringConcat[p-1] == '0') && p >= 0)
+            while ((stringConcat[p - 1] == '0') && p >= 0)
             {
                 p--;
             }
 
             stringConcat = stringConcat.Substring(0, p);
 
-            return Convert.ToDecimal(stringConcat);
+            var result = (value < 0 ? Convert.ToDecimal(stringConcat) * -1 : Convert.ToDecimal(stringConcat));
+            return result;
         }
 
         public static string RealTrunckToString(this decimal value, int number, bool forceZeros = false)
@@ -74,21 +86,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            List<decimal> lst = new List<decimal>
+            var lst = new SortedList<decimal, decimal>()
             {
-                10.45678m
-                ,0.45678m
-                ,10.45m
-                ,10m
-                ,.45m
-                ,10.456999999m
-                ,1012345.456999999m
+                {10.456789m, 10.4567m }
+                , {10.45678m, 10.4567m }
+                , {0.45678m, 0.4567m }
+                , {10.45m, 10.45m }
+                , {10m, 10m }
+                , {.45m, 0.45m }
+                , {10.456999999m, 10.4569m }
+                , {1012345.456999999m, 1012345.4569m }
+                , {1m, 1m }
+                , {0m, 0m }
+                , {-1m, -1m }
             };
 
             foreach (var item in lst)
             {
+                //Assert.AreEqual(item.Key.RealTrunck(4), item.Value);
+            }
+
+            foreach (var item in lst)
+            {
                 //Console.WriteLine("V: {0} |RealTrunck= {1} |RealTrunckToString= {2} |ForceZeroOn= {3} |N4= {4}", item, item.RealTrunck(4), item.RealTrunckToString(4, true), item.RealTrunckToString(4, false), item.ToString("N4"));
-                Console.WriteLine("Va= {0} |RealTrunck= {1} |RealTrunckToString= {2} |ForceZeroOn= {3} |N4= {4}", item, item.RealTrunck(4), item.RealTrunckToString(4, true), item.RealTrunckToString(4, false), item.ToString("N4"));
+                Console.WriteLine("Va= {0} |RealTrunck= {1} |RealTrunckToString= {2} |ForceZeroOn= {3} |N4= {4}", item.Key , item.Key.RealTrunck(4), item.Key.RealTrunckToString(4, true), item.Key.RealTrunckToString(4, false), item.Key.ToString("N4"));
                 Console.WriteLine(new string('-', 80));
             }
 
---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRealTrunck()
+        {
+            // Massa de teste em 4 casas
+            var lst = new SortedList<decimal, decimal>()
+            {
+                {10.456789m, 10.4567m }
+                , {10.45678m, 10.4567m }
+                , {0.45678m, 0.4567m }
+                , {10.45m, 10.45m }
+                , {10m, 10m }
+                , {.45m, 0.45m }
+                , {10.456999999m, 10.4569m }
+                , {1012345.456999999m, 1012345.4569m }
+                , {1012345.0004999999m, 1012345.0004m }
+                , {1m, 1m }
+                , {0m, 0m }
+                , {-1m, -1m }
+                , {-10.456789m, -10.4567m }
+                , {-10.45678m, -10.4567m }
+                , {-0.45678m, -0.4567m }
+                , {-10.45m, -10.45m }
+                , {-10m, -10m }
+                , {-.45m, -0.45m }
+                , {-10.456999999m, -10.4569m }
+                , {-1012345.456999999m, -1012345.4569m }
+                , {-1012345.0004999999m, -1012345.0004m }
+            };
+
+            foreach (var item in lst)
+            {
+                Assert.AreEqual(item.Value, item.Key.RealTrunck(4));
+            }
+
+        }
+
+        [TestMethod]
+        public void RealTrunckToString()
+        {
+            char separator = Convert.ToChar(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+            // Massa de teste em 4 casas
+            var lst = new SortedList<decimal, string>()
+            {
+                {10.456789m, "10.4567".Replace('.', separator) }
+                , {10.45678m, "10.4567".Replace('.', separator) }
+                , {0.45678m, "0.4567".Replace('.', separator) }
+                , {10.45m, "10.45".Replace('.', separator) }
+                , {10m, "10".Replace('.', separator) }
+                , {.45m, "0.45".Replace('.', separator) }
+                , {10.456999999m, "10.4569".Replace('.', separator) }
+                , {1012345.456999999m, "1012345.4569".Replace('.', separator) }
+            };
+
+            foreach (var item in lst)
+            {
+                Assert.AreEqual(item.Value, item.Key.RealTrunckToString(4));
+            }
+
+        }
+    }
+}
